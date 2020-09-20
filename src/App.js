be82a8ca83a9c197dda59ceed9d1abc20b544b89
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-20 17:50:53 -0400

    Expressions and conditionals in JSX

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component{
+
   render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
+
     return (
-      <div className="App">
-        <h1>Hello from React</h1>
+      <div className='App'>
+        {loading ? <h4>Loading...</h4> : <h1> Hello {showName && name}</h1>} 
+        {/* if loading then display loading if not then show name */}
+        {/* if showName is true then show name, if false then don't */}
       </div>
-    );
+    )
   }
 }
-
+  
 export default App;
