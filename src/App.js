8121e16dbcb20116c44ac6935ed76b50b4cd81e5
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-20 22:25:33 -0400

    Nav bar components and props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component{
 
   render() {
-    const name = 'John Doe';
-    const loading = false;
-    const showName = true;
 
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1> Hello {showName && name}</h1>} 
-        {/* if loading then display loading if not then show name */}
-        {/* if showName is true then show name, if false then don't */}
+        <Navbar />
       </div>
     )
   }
