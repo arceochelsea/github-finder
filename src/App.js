52f44ff5e9c51d7518fe2552e75fd1142229496e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-23 22:47:48 -0400

    HTTP Reqs and Updating State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component{
+  state = {
+    users: [],
+    loading: false
+  }
 
-  render() {
+  async componentDidMount() { //will run when the component mounts, if you ever want to make a HTTP req to GH right when the app loads THIS is where you want to do it.
+    
+    this.setState({loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false});
+  }
+
+  render() { //life cycle method
 
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users}/> 
+        {/* users compm were passing in loading and users from our state, were passing in as props */}
         </div>
       </div>
     )
